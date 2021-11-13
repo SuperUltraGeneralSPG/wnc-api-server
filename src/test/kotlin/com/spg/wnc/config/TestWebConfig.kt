12f764bf9a4427490df3f6a4f2354ff7d3d393f2
SJAhn <seongjin.dev@gmail.com>
2021-11-14 01:35:37 +0900

    TestWebConfig JsonMapper 추가

--- src/test/kotlin/com/spg/wnc/config/TestWebConfig.kt
+++ src/test/kotlin/com/spg/wnc/config/TestWebConfig.kt
@@ -1,11 +1,16 @@
 package com.spg.wnc.config
 
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.nhaarman.mockitokotlin2.mock
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 
 @TestConfiguration
-@Import(value = [WebConfig::class])
+@Import(value = [WebConfig::class, SerializationConfig::class])
 class TestWebConfig {
-
+    @Bean
+    fun jsonMapper(): JsonMapper {
+        return mock()
+    }
 }
