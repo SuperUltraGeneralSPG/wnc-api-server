4ce8b40b768d8fcd4cbeb1643823d8f504fca59a
SJAhn <seongjin.dev@gmail.com>
2021-11-14 19:45:58 +0900

    getInfo Response에 이름 추가

--- src/main/kotlin/com/spg/wnc/api/message/response/UserInfoResponse.kt
+++ src/main/kotlin/com/spg/wnc/api/message/response/UserInfoResponse.kt
@@ -8,6 +8,7 @@ data class UserInfoResponse(
     val id: Long,
     val loginId: String,
     val password: String,
+    val name: String,
     val userType: UserType,
     val createdAt: LocalDateTime,
     val career: List<String>?
@@ -17,6 +18,7 @@ data class UserInfoResponse(
             id = user.id,
             loginId = user.loginId,
             password = user.password,
+            name = user.name,
             userType = user.userType,
             createdAt = user.createdAt,
             career = career
